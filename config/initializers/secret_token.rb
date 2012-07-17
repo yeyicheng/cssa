668fc4f528151f10c80e231da81a8cfc0959283b
Yicheng Ye <ye.yicheng123@gmail.com>
2012-07-17 00:54:03 -0400

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cssa::Application.config.secret_token = '3340a0b7346bf16d1f6060bf25c7fcd16c9aaf07165559b66b49573e9fde32f3af4ae893c4cfc8ca34387c2293cf2cff104afda2dafd173373a27345b283797e'
