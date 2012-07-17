dca6cbdbf01e0b5fb2615649edc0dbeb9a8386dd
Yicheng Ye <ye.yicheng123@gmail.com>
2012-07-17 08:21:20 -0400

    secure pwd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,58 @@
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
-  
-  email_regex = /^[\w+\-._]+@[a-z\d\-._]+\.[a-z]+$/i
-  
-  validates :name, :presence => true, 
-				   :length => { :maximum => 30, :minimum => 5 }
+	attr_accessor :password
+	
+	attr_accessible :email, :name, :password, :password_confirmation
+
+	email_regex = /^[\w+\-._]+@[a-z\d\-._]+\.[a-z]+$/i
+
+	validates :name, :presence => true, 
+					 :length => { :maximum => 30, :minimum => 5 }
 				   
-  validates :email, :presence => true, 
-					:format => { :with => email_regex },
-					:uniqueness => { :case_sensitive => false }
-  
+	validates :email, :presence => true, 
+					  :format => { :with => email_regex },
+					  :uniqueness => { :case_sensitive => false }
+
+	validates :password, :confirmation => true,
+						 :length => { :within => 6..40 }
+						 
+	before_save :encrypt_password
+	
+	# Return true if the user's password matches the submitted password.
+	def password_matched? (submitted_password)
+		encrypted_password == encrypt(submitted_password)
+	end
+	
+	private
+	
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+		
+		def encrypt_password
+			self.salt = make_salt if new_record?
+			self.encrypted_password = encrypt(password)
+		end
+		
+		def encrypt(pwd)
+			secure_hash("#{salt}--#{pwd}")
+		end
+		
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
+
 end
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime        not null
+#  updated_at         :datetime        not null
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
