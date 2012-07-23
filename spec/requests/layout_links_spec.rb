83e3a3d660e9ff4b62648afb87a3bc899dbdf36d
Yicheng Ye <yyc@ubuntu.(none)>
2012-07-23 12:15:13 -0400

    ..

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,32 +1,32 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-  
-  describe "GET /layout_links" do
-    it "should have a Home page at '/'" do
-		get '/'
-		response.should have_selector('title', :content => "Home")
+
+	describe "GET /layout_links" do
+		it "should have a Home page at '/'" do
+			get '/'
+			response.should have_selector('title', :content => "Home")
+		end
+		it "should have a Clubs page at '/club'" do
+			get '/club'
+			response.should have_selector('title', :content => "Clubs")
+		end
+		it "should have an About page at '/contact'" do
+			get '/contact'
+			response.should have_selector('title', :content => "Contact")
+		end
+		it "should have an Contact page at '/about'" do
+			get '/about'
+			response.should have_selector('title', :content => "About")
+		end
+		it "should have a signup page at '/sign_up'" do
+			get '/sign_up'
+			response.should have_selector('title', :content => "Sign up")
+		end
+		it "should have a signup page at '/sign_in'" do
+			get '/sign_in'
+			response.should have_selector('title', :content => "Sign in")
+		end
 	end
-	it "should have a Clubs page at '/club'" do
-		get '/club'
-		response.should have_selector('title', :content => "Clubs")
-	end
-	it "should have an About page at '/contact'" do
-		get '/contact'
-		response.should have_selector('title', :content => "Contact")
-	end
-	it "should have an Contact page at '/about'" do
-		get '/about'
-		response.should have_selector('title', :content => "About")
-	end
-	it "should have a signup page at '/sign_up'" do
-		get '/sign_up'
-		response.should have_selector('title', :content => "Sign up")
-	end
-	it "should have a signup page at '/sign_in'" do
-		get '/sign_in'
-		response.should have_selector('title', :content => "Sign in")
-	end
-  end
   
 end
