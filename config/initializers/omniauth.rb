86cc3bfed3b3410f3482491170610aafc11d807e
yeyicheng <ye.yicheng123@gmail.com>
2012-08-17 11:02:50 -0400

    image tag

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.full_host = "http://hollow-waterfall-1839.herokuapp.com"
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, '275830165859773', '43bc9e3a7a14dfe69623c0070a32f600',
-	{:scope => 'user_groups,friends_groups', :callback_path => '/' }
+	{:scope => 'user_groups,friends_groups', :display => 'popup', :callback_path => '/' }
 end
 
 Rails.application.config.middleware.use Rack::Session::Cookie do
