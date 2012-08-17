a1c2697e752a830207bc4a873dc01061047c9121
yeyicheng <ye.yicheng123@gmail.com>
2012-08-16 22:43:34 -0400

    re

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV['275830165859773'], ENV['43bc9e3a7a14dfe69623c0070a32f600'],
+	provider :facebook, '275830165859773', '43bc9e3a7a14dfe69623c0070a32f600',
 			{:scope => 'user_groups, friends_groups', :display => 'popup'}
 end
